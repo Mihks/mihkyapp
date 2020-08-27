fdefa4c898aec8457486c5a671da8666979799fd
Mihks <34143350+Mihks@users.noreply.github.com>
2020-08-27 17:28:54 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>essai</title>
+</head>
+<body>
+
+
+
+<button>Ajax</button>
+
+<div></div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.1/jquery.min.js"></script>
+		<script src=""https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery.ui.min.js""></script>
+
+
+		<script> 
+			
+		$(function () {
+
+
+			$.post('Ajax.php',{un:'4'},function(data){
+
+				$('div').html(data);
+			});
+							// body...
+						}	);
+
+		</script> 
+			
+</body>
+</html>
+
