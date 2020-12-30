82f16184e24a6fa831ec35a915cdcdc7d8ed9de1
Mihks <34143350+Mihks@users.noreply.github.com>
2020-12-30 22:23:58 +0100

    Create contact.php

---
+++ agence/includes/contact.php
@@ -0,0 +1,17 @@
+<article>
+
+				<div id="contact">
+
+					<h2>Contact</h2>
+
+					<div class="centre-text">
+						<p>Pour avoir des informations diverses notamment dans le cadre d’un partenariat, veuillez nous contacter par courriel ou par téléphone.</p>
+
+						<p>Courriel :<a href="mailto:kleinmoudiba@gmail.com"> kleinmoudiba@gmail.com</a></p>
+
+						<p>Tel : <a href="tel:+241 74 87 21 20"> +241 74 87 21 20</a></p>
+						<p>Tel : <a href="tel:+241 77 67 37 88"> +241 77 67 37 88</a></p>
+
+					</div>
+				</div>
+			</article>
