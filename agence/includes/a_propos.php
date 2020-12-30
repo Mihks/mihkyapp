d0b282d21ea03f81b578cfd6c5fea45af80baa0c
Mihks <34143350+Mihks@users.noreply.github.com>
2020-12-30 22:22:52 +0100

    Create a_propos.php

---
+++ agence/includes/a_propos.php
@@ -0,0 +1,17 @@
+<article>
+
+				<div id="a_propos">
+
+
+					<h2>A propos de</h2>
+
+					<div class="centre-text">
+						<p>Flemard est un service informatique indépendant <b> de mise en rélation</b> affilié à <b> AZOBE S.A.R.L</b> réconnue d’utilité publique . Son objectif fondamental est d’aider au mieux des individus au quotidien dans des secteurs d’activités variés, en apportant des solutions applicatives dynamiques et scalables . Pour débuter son action, <b>Flemard</b> se lance dans le secteur du transport en proposant une solution dans la réservation des billets. <b>Flemard</b> souhaite et est disposé à travailler en <i style="color: red;font-weight: bolder;">symbiose</i> avec tout <b>agence de voyage</b> routière et/ou maritime .</p>
+
+						<p>Réservez vos places pour un voyage de Libreville vers l'interieur du Gabon et vise versa avec votre ordinateur, téléphones…</p>
+					</div>
+
+					<img src="agence/images/flemard.jpg" id="imaga" width="200" height="200" border="1" title="Flemard" alt="Flemard"  /><sub style="font-family: chiller;color: red;">Flemard</sub>
+					
+				</div>
+			</article>
