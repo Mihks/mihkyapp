e8290f14f972b2d2e97fe17f34454a82d087d31c
Mihks <34143350+Mihks@users.noreply.github.com>
2021-01-29 03:51:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,181 +1,42 @@
-<? php
+<?php
+// Read the variables sent via POST from our API
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text        = $_POST["text"];
 
-// Lit les variables envoyées via POST
+if ($text == "") {
+    // This is the first request. Note how we start the response with CON
+    $response  = "CON What would you want to check \n";
+    $response .= "1. My Account \n";
+    $response .= "2. My phone number";
 
-$ sessionId = $ _POST ["sessionId"];
+} else if ($text == "1") {
+    // Business logic for first level response
+    $response = "CON Choose account information you want to view \n";
+    $response .= "1. Account number \n";
+    $response .= "2. Account balance";
 
-$ serviceCode = $ _POST ["serviceCode"];
+} else if ($text == "2") {
+    // Business logic for first level response
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your phone number is ".$phoneNumber;
 
-$ text = $ _POST ["text"];
+} else if($text == "1*1") { 
+    // This is a second level response where the user selected 1 in the first instance
+    $accountNumber  = "ACC1001";
 
-// Ceci est le premier écran de menu
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your account number is ".$accountNumber;
 
-si ($ text == "") {
+} else if ( $text == "1*2" ) {
+    // This is a second level response where the user selected 1 in the first instance
+    $balance  = "KES 10,000";
 
-$ response = "CON Bonjour, je peux vous aider avec la réservation d'événement \ n";
-
-$ response. = "1. Entrez 1 pour continuer";
-
-}
-// Menu pour un utilisateur qui sélectionne '1' dans le premier menu
-
-// Sera amené à ce deuxième écran de menu
-
-sinon si ($ text == "1") {
-
-$ response = "CON Choisissez une table pour la réservation ci-dessous \ n ";
-
-$ response. = "1. Table pour 2 \ n";
-
-$ response. = "2. Table pour 4 \ n";
-
-$ response. = "3. Table pour 6 \ n";
-
-$ response. = "4. Table pour 8 \ n";
-
-}
-// Menu pour un utilisateur qui sélectionne '1' dans le deuxième menu ci-dessus
-
-// Sera amené à ce troisième écran de menu
-
-sinon si ($ text == "1 * 1") {
-
-$ response = "CON Vous êtes sur le point de réserver une table pour 2 \ n ";
-
-$ response. = "Veuillez entrer 1 pour confirmer \ n";
-
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your balance is ".$balance;
 }
 
-else if ($ text == "1 * 1 * 1") {
-
-$ response = "CON Table for 2 cost -N- 50,000.00 \ n";
-
-$ response. = "Entrez 1 pour continuer \ n";
-
-$ response. = "Entrez 0 pour annuler";
-
-}
-else if ($ text == "1 * 1 * 1 * 1") {
-
-$ response = "END Votre réservation de table pour 2 a été réservée";
-
-}
-
-else if ($ text == "1 * 1 * 1 * 0") {
-
-$ response = "
-
-// Menu pour un utilisateur qui sélectionne "2" dans le deuxième menu ci-dessus
-
-// Sera amené à ce quatrième écran de menu
-
-sinon si ($ text == "1 * 2") {
-
-$ response = "CON Vous êtes sur le point de réserver une table pour 4 \ n ";
-
-$ response. = "Veuillez entrer 1 pour confirmer \ n";
-
-}
-// Menu pour un utilisateur qui sélectionne "1" dans le quatrième écran de menu
-
-sinon si ($ text == "1 * 2 * 1") {
-
-$ response = "CON Table for 4 cost -N- 150,000.00 \ n";
-
-$ response. = "Entrez 1 pour continuer \ n";
-
-$ response. = "Entrez 0 pour annuler";
-
-}
-
-else if ($ text == "1 * 2 * 1 * 1") {
-
-$ response = "END Votre réservation de table pour 4 a été réservée";
-
-}
-
-else if ($ text == "
-
-}
-
-// Menu pour un utilisateur qui entre "3" dans le deuxième menu ci-dessus
-
-// Sera amené à ce cinquième écran de menu
-
-sinon si ($ text == "1 * 3") {
-
-$ response = "CON Vous êtes sur le point de réserver une table pour 6 \ n ";
-
-$ response. = "Veuillez entrer 1 pour confirmer \ n";
-
-}
-
-// Menu pour un utilisateur qui entre "1" dans le cinquième menu
-
-sinon si ($ text == "1 * 3 * 1") {
-
-$ response = "CON Table for 6 cost -N- 250,000.00 \ n";
-
-$ response. = "Entrez 1 pour continuer \ n";
-
-$ response. = "Entrez 0 pour annuler";
-
-}
-
-else if ($ text == "1 * 3 * 1 * 1") {
-
-$ response = "END Votre réservation de table pour 6 a été réservée";
-
-}
-
-else if ($ text == "
-
-$ response = "END Votre réservation de table pour 6 a été annulée";
-
-}
-
-// Menu pour un utilisateur qui entre "4" dans le deuxième menu ci-dessus
-
-// Sera amené à ce sixième écran de menu
-
-sinon si ($ text == "1 * 4") {
-
-$ response = "CON Vous êtes sur le point de réserver une table pour 8 \ n ";
-
-$ response. = "Veuillez entrer 1 pour confirmer \ n";
-
-}
-
-// Menu pour un utilisateur qui entre "1" dans le sixième menu
-
-sinon si ($ text == "1 * 4 * 1") {
-
-$ response = "CON Table for 8 cost -N- 250,000.00 \ n";
-
-$ response. = "Entrez 1 pour continuer \ n";
-
-$ response. = "Entrez 0 pour annuler";
-
-}
-
-else if ($ text == "1 * 4 * 1 * 1") {
-
-$ response = "
-
-else if ($ text == "1 * 4 * 1 * 0") {
-
-$ response = "END Votre réservation de table pour 8 a été annulée";
-
-}
-
-// en-
-
-tête de
-
-réponse d'écho
-
-('Content-type: text / plain');
-
-echo $ response
-
-?>
+// Echo the response back to the API
+header('Content-type: text/plain');
+echo $response;
