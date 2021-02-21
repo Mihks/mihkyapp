bdfc4da52d416dfbf8059d90c818d4a3964fa7cb
Mihks <34143350+Mihks@users.noreply.github.com>
2021-02-21 13:48:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if ($text == "") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
     $place = array();
-    $response = "END Nombre de place ".$text$text;
+    $response = "END Nombre de place ".$text;
     
 } else if($text == "1*1") { 
     // This is a second level response where the user selected 1 in the first instance
